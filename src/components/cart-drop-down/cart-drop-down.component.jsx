b1c0c4f3451747dc91f02437eb4885c116140481
amirhossein safari <amirhosseinsafari@amirhosseins-MacBook-Air.local>
2023-10-21 10:08:54 +0330

    use reselect in our app

--- src/components/cart-drop-down/cart-drop-down.component.jsx
+++ src/components/cart-drop-down/cart-drop-down.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 import "./cart-drop-down.styles.scss";
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -16,7 +17,11 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   cartItems,
+// });
+
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 export default connect(mapStateToProps)(CartDropdown);
