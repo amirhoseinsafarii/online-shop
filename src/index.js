34a52be3c22dcafd26af8214d015200cdf923b05
amirhossein safari <amirhosseinsafari@amirhosseins-MacBook-Air.local>
2023-10-25 10:36:42 +0330

    adding redux-persist to persist our cart reducer across sessions

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
-import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
